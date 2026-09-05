0af4240abf84e3d4823fbe80a29dff6b2f82be64
Sameer <jangidsameer77@gmail.com>
2026-09-05 23:52:24 +0530

    Initialize backend project with Express, MongoDB, and dotenv configuration. Added .gitignore, package.json, and essential server setup in index.js. Implemented basic app structure with user routes.

---
+++ index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = () => {
+  connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  });
+};
+
+startServer();
---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.use("/api/v1/users", userRoutes);
+
+export default app;
